d1458870791f4447ec67b6f3d0058a3f51152af0
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-06-21 16:23:18 +0530

    mongodb_setup done

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
 const app = express();
-
+//env
 dotenv.config();
-// rest api
 
+//database connect
+connectDB();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// API
 app.get("/", (req, res) => {
   res.send("<h1>welcome to Avinabh E-commerce app</h1>");
 });
